9c747dd1184f2d4e2423e359fa21142915fe363e
ThiagoFPMR <thiago.rodrigues.056@ufrn.edu.br>
2023-03-26 18:50:29 -0300

    more tests

--- list/arrayList_test.go
+++ list/arrayList_test.go
@@ -18,3 +18,29 @@ func TestLength(t *testing.T) {
 		t.Errorf("Length of ArrayList is %d, expected %d", arrayList.Length(), limit)
 	}
 }
+
+func TestRemoveFromBack(t *testing.T) {
+	var arrayList ArrayList
+	arrayList.Init()
+
+	arrayList.AddToBack(i)
+	arrayList.RemoveFromBack()
+
+	if arrayList.Length() > 0 {
+		t.Errorf("Failed to remove element. Length of ArrayList is %d, expected %d", arrayList.Length(), limit-1)
+	}
+}
+
+func TestAddOnIndex(t *testing.T) {
+	var arrayList ArrayList
+	arrayList.Init()
+
+	arrayList.AddToBack(1)
+	arrayList.AddToBack(2)
+	arrayList.AddToBack(3)
+	arrayList.AddOnIndex(1, 4)
+
+	if arrayList.Get(1) != 4 {
+		t.Errorf("Failed to add element on index 1. Element at index 1 is %d, expected %d", arrayList.Get(1), 4)
+	}
+}
